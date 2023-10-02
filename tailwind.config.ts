f7b19f96e715ef159de478110fb8d31161c6920f
kevinanielsen <keveran@gmail.com>
2023-10-02 11:51:43 +0200

    style: implement color palette and text

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,29 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontSize: {
+        sm: "0.750rem",
+        base: "1rem",
+        xl: "1.333rem",
+        "2xl": "1.777rem",
+        "3xl": "2.369rem",
+        "4xl": "3.158rem",
+        "5xl": "4.210rem",
+      },
+      fontFamily: {
+        heading: "Poppins",
+        body: "Poppins",
+      },
+      fontWeight: {
+        normal: "400",
+        bold: "700",
+      },
+      colors: {
+        text: "var(--text)",
+        background: "var(--background)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        accent: "var(--accent)",
       },
     },
   },
