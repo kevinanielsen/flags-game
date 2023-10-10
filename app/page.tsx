38bbd01d1bc52f3adcc7ea2f95750a74ef7320c9
Kevin Nielsen <kevinanielsen@outlook.com>
2023-10-10 15:52:19 +0200

    feat add functionality to start game button (#17)
    * chore: update deps
    
    * feat: change button to link to /game

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import LeaderBoard from "@/components/leaderboard";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="absolute w-full h-[90%] flex justify-center items-center">
-      <div className="flex justify-center items-center gap-8 w-full max-w-2xl">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-8 w-full max-w-2xl m-4 mt-12">
         <div className="border w-full grid justify-center items-center h-72 rounded-md">
-          <Button className="text-base" size="lg">
+          <Link
+            className="text-base font-bold bg-primary dark:text-primary dark:bg-text px-4 py-3 rounded-md"
+            href="/game"
+          >
             Start Game
-          </Button>
+          </Link>
         </div>
         <LeaderBoard />
       </div>
