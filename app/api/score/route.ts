14292ac839524d95e6c35d0aa26bfb522466d003
kevinanielsen <keveran@gmail.com>
2023-11-05 11:27:50 +0100

    fix(api): fix response json

--- app/api/score/route.ts
+++ app/api/score/route.ts
@@ -1,5 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
 
 interface IBody {
   score_count: number;
@@ -7,32 +7,25 @@ interface IBody {
   seconds_spent: number;
 }
 
+type ResponseData = {
+  message: string;
+};
+
 export async function POST(req: NextRequest) {
   try {
     const body: IBody = await req.json();
     const { score_count, user_name, seconds_spent } = body;
 
     if (score_count === null || score_count === undefined) {
-      return NextResponse.json(
-        { error: "Missing score_count in body" },
-        { status: 400 },
-      );
+      return NextResponse.json("Missing score_count in body", { status: 400 });
     }
     if (!user_name) {
-      return NextResponse.json(
-        {
-          error: "Missing user_name in body",
-        },
-        { status: 400 },
-      );
+      return NextResponse.json("Missing user_name in body", { status: 400 });
     }
     if (!seconds_spent) {
-      return NextResponse.json(
-        {
-          error: "Missing seconds_spent in body",
-        },
-        { status: 400 },
-      );
+      return NextResponse.json("Missing seconds_spent in body", {
+        status: 400,
+      });
     }
 
     const dbEntry = await prisma.score.create({
