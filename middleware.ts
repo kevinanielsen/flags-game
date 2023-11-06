43971de407d1cd5e219b3cef96a67f79ea39043f
kevinanielsen <keveran@gmail.com>
2023-11-06 08:43:00 +0100

    fix(api): fix ratelimit api

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,19 @@
 import newRateLimit from "@/lib/ratelimit";
 import { NextRequest, NextResponse } from "next/server";
+import { headers } from "next/headers";
 
 const ratelimit = newRateLimit();
 
 export async function middleware(request: NextRequest) {
-  const ip = request.headers.get("x-forwarded-for") ?? "";
+  const headersList = headers();
+  const ip = headersList.get("x-forwarded-for") ?? "127.0.0.1";
+
   const { success, reset } = await ratelimit.limit(ip);
 
   if (!success) {
     const now = Date.now();
     const retryAfter = Math.floor((reset - now) / 1000);
-    return new NextResponse("Too many requests", {
+    return new NextResponse(`Too many requests. IP: ${ip}`, {
       status: 429,
       headers: {
         ["retry-after"]: `${retryAfter}`,
