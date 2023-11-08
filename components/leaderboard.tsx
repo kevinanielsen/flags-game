46231a60effdbfc8872cd7accf499f3aa312078d
kevinanielsen <kevinanielsen@outlook.com>
2023-11-08 16:43:14 +0100

    feat: add react-query to load leaderboard

--- components/leaderboard.tsx
+++ components/leaderboard.tsx
@@ -1,36 +1,68 @@
+"use client";
+
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
-import { Spinner } from "@nextui-org/react";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { TScore } from "@/types/Score";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const data = Array.from({ length: 50 }).map((_, i) => `${i}. John Doe`);
+const initialData: TScore[] = [
+  {
+    created_at: new Date(),
+    score_count: 500,
+    score_id: "Test",
+    seconds_spent: 500,
+    user_name: "Test",
+  },
+];
 
-const LeaderBoard = async () => {
-  const queryClient = useQueryClient();
-
-  const { data, isLoading, refetch } = useQuery({
+const LeaderBoard = () => {
+  const { data, isLoading, error } = useQuery({
     queryKey: ["top-scores"],
-    queryFn: async () => await axios.get("/api/top-scores"),
+    queryFn: async () => {
+      const res = await axios.get<TScore[]>("/api/top-scores");
+      return res.data;
+    },
   });
 
-  if (isLoading) return <Spinner />;
-
-  return (
-    <ScrollArea className="w-full h-72 rounded-md border">
-      <div className="p-4">
-        <h3 className="mb-4 text-base font-medium leading-none">Leaderboard</h3>
-        {data.map((name) => (
-          <>
-            <div key={name} className="text-sm">
-              {name}
-            </div>
-            <Separator className="my-2" />
-          </>
-        ))}
+  if (isLoading)
+    return (
+      <div className="border w-full grid justify-center items-center h-72 rounded-md text-text">
+        <span>Loading...</span>
       </div>
-    </ScrollArea>
-  );
+    );
+
+  if (error) {
+    console.log(error);
+    return (
+      <div className="border w-full grid justify-center items-center h-72 rounded-md">
+        An error loading the leaderboard occured!
+      </div>
+    );
+  }
+
+  if (data)
+    return (
+      <ScrollArea className="w-full h-72 rounded-md border">
+        <div className="p-4">
+          <h3 className="mb-4 text-base font-medium leading-none">
+            Leaderboard
+          </h3>
+          {data.map((score) => (
+            <>
+              <div
+                className="text-sm flex justify-between"
+                key={score.score_id}
+              >
+                <p>{score.user_name}</p>
+                <p>{score.score_count}</p>
+              </div>
+              <Separator className="my-2" />
+            </>
+          ))}
+        </div>
+      </ScrollArea>
+    );
 };
 
 export default LeaderBoard;
