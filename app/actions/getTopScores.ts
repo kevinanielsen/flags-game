0542af53ed20607cb0bc456a268dafde0cf92f55
kevinanielsen <keveran@gmail.com>
2023-11-10 15:03:56 +0100

    refactor: add types to getTopScores

--- app/actions/getTopScores.ts
+++ app/actions/getTopScores.ts
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma";
+import { TScore } from "@/types/Score";
 
-const getTopScores = async () => {
+const getTopScores: () => Promise<TScore[] | Error> = async () => {
   const top_scores = await prisma.score.findMany({
     take: 100,
     orderBy: {
@@ -9,20 +10,24 @@ const getTopScores = async () => {
   });
 
   if (top_scores) {
-    const TopScores = top_scores.filter((score) => score.score_count === 193);
-
-    const notTopScores = top_scores.filter(
-      (score) => score.score_count !== 193,
+    const TopScores: TScore[] = top_scores.filter(
+      (score: TScore) => score.score_count === 193,
     );
 
-    const sortedTopScores = TopScores.sort(
-      (a, b) => a.seconds_spent - b.seconds_spent,
+    const notTopScores: TScore[] = top_scores.filter(
+      (score: TScore) => score.score_count !== 193,
     );
 
-    const sortedScores = [...sortedTopScores, ...notTopScores];
+    const sortedTopScores: TScore[] = TopScores.sort(
+      (a: TScore, b: TScore) => a.seconds_spent - b.seconds_spent,
+    );
+
+    const sortedScores: TScore[] = [...sortedTopScores, ...notTopScores];
 
     return sortedScores;
   }
+
+  return new Error("Error getting topScores");
 };
 
 export default getTopScores;
