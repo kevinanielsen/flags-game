1301cbea00cc2f15642f069606c2cb4792a3fe83
kevinanielsen <keveran@gmail.com>
2023-11-10 14:24:35 +0100

    test: add test for getTopScores action

---
+++ tests/actions/getTopScores.test.ts
@@ -0,0 +1,18 @@
+import getTopScores from "@/app/actions/getTopScores";
+
+test("getTopScores function", async () => {
+  const expected = {
+    score_id: "654bf0264ee46b3abe02802d",
+    score_count: 191,
+    user_name: "TOP_SCORE",
+    created_at: "2023-11-10T12:50:04.146Z",
+    seconds_spent: 1,
+  };
+
+  const response = await getTopScores();
+  if (response) {
+    const result = response[0];
+
+    expect(expected.score_id).toBe(result.score_id);
+  }
+});
