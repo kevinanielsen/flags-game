abe6c94efe8ba60c3061e7cbc74a7c9809cdbe8d
kevinanielsen <keveran@gmail.com>
2023-11-19 16:57:29 +0100

    refactor: add nested test

--- tests/actions/getTopScores.test.ts
+++ tests/actions/getTopScores.test.ts
@@ -1,18 +1,21 @@
 import getTopScores from "@/app/actions/getTopScores";
 
-test("getTopScores function", async () => {
-  const expected = {
-    score_id: "654bf0264ee46b3abe02802d",
-    score_count: 191,
-    user_name: "TOP_SCORE",
-    created_at: "2023-11-10T12:50:04.146Z",
-    seconds_spent: 1,
-  };
+describe("getTopScores function", () => {
+  test("Returns the best score at index 0", () => {
+    const expected = {
+      score_id: "654bf0264ee46b3abe02802d",
+      score_count: 191,
+      user_name: "TOP_SCORE",
+      created_at: "2023-11-10T12:50:04.146Z",
+      seconds_spent: 1,
+    };
 
-  const response = await getTopScores();
-  if (response instanceof Array) {
-    const result = response[0];
+    getTopScores().then((response) => {
+      if (response instanceof Array) {
+        const result = response[0];
 
-    expect(expected.score_id).toBe(result.score_id);
-  }
+        expect(expected.score_id).toBe(result.score_id);
+      }
+    });
+  });
 });
